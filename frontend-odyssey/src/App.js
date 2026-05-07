3a8aa6bb809a6bb713afbd55985e74ee3bc5c91a
Anamaria Brechler <brechleranamaria13@gmail.com>
2026-05-07 21:25:27 +0300

    am adaugat butonul pentru Light/Dark Mode

--- frontend-odyssey/src/App.js
+++ frontend-odyssey/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import Acasa from "./pagini/Acasa";
 import Pisici from "./pagini/Pisici";
@@ -10,25 +11,29 @@ import FavoriteCaini from "./pagini/FavoriteCaini";
 import FavoriteGlume from "./pagini/FavoriteGlume";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const stilGlobal = {
+    backgroundColor: darkMode ? "#222" : "#fff",
+    color: darkMode ? "#fff" : "#000",
+    minHeight: "100vh",
+    transition: "0.3s"
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Acasa />} />
-
-        <Route path="/pisici" element={<Pisici />} />
-
-        <Route path="/caini" element={<Caini />} />
-
-        <Route path="/glume" element={<Glume />} />
-
-        <Route path="/favorite-pisici" element={<FavoritePisici />} />
-
-        <Route path="/favorite-caini" element={<FavoriteCaini />} />
-
-        <Route path="/favorite-glume" element={<FavoriteGlume />} />
-      </Routes>
-    </BrowserRouter>
+    <div style={stilGlobal}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Acasa darkMode={darkMode} setDarkMode={setDarkMode} />} />
+          <Route path="/pisici" element={<Pisici />} />
+          <Route path="/caini" element={<Caini />} />
+          <Route path="/glume" element={<Glume />} />
+          <Route path="/favorite-pisici" element={<FavoritePisici />} />
+          <Route path="/favorite-caini" element={<FavoriteCaini />} />
+          <Route path="/favorite-glume" element={<FavoriteGlume />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- frontend-odyssey/src/pagini/Acasa.js
+++ frontend-odyssey/src/pagini/Acasa.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 
-function Acasa() {
+function Acasa({ darkMode, setDarkMode }) {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>Frontend Odyssey</h1>
 
+      <button onClick={() => setDarkMode(!darkMode)}>
+        Schimba la {darkMode ? "Light" : "Dark"} Mode
+      </button>
+
+      <hr />
+      
       <h2>Alege o categorie</h2>
 
       <Link to="/pisici">
@@ -42,11 +48,9 @@ function Acasa() {
       <br />
       <br />
 
-      {/* Butonul nou pentru glume favorite */}
       <Link to="/favorite-glume">
         <button>Glume Favorite</button>
       </Link>
-
     </div>
   );
 }
