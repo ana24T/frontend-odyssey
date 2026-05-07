98e0f9aee61fe98b356b677531544ee713790466
Anamaria Brechler <brechleranamaria13@gmail.com>
2026-05-07 19:54:49 +0300

    am creat pagina acasa

---
+++ frontend-odyssey/src/pagini/Acasa.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function Acasa() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Frontend Odyssey</h1>
+
+      <h2>Alege o categorie</h2>
+
+      <Link to="/pisici">
+        <button>Pisici</button>
+      </Link>
+
+      <br />
+      <br />
+
+      <Link to="/caini">
+        <button>Caini</button>
+      </Link>
+
+      <br />
+      <br />
+
+      <Link to="/glume">
+        <button>Glume</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Acasa;
