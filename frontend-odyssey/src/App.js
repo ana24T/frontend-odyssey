0f202a2a42065a33190534aa0acf3d3c098aa03a
Anamaria Brechler <brechleranamaria13@gmail.com>
2026-05-07 20:05:59 +0300

    am separat aplicatia pe pagini

--- frontend-odyssey/src/App.js
+++ frontend-odyssey/src/App.js
@@ -1,52 +1,25 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Acasa from "./pagini/Acasa";
+import Pisici from "./pagini/Pisici";
+import Caini from "./pagini/Caini";
+import Glume from "./pagini/Glume";
 
 function App() {
-  const [cat, setCat] = useState("");
-  const [joke, setJoke] = useState("");
-  const [dog, setDog] = useState("");
-
-  const getCat = async () => {
-    const res = await fetch("https://api.thecatapi.com/v1/images/search");
-    const data = await res.json();
-    setCat(data[0].url);
-  };
-
-  const getJoke = async () => {
-    const res = await fetch("https://official-joke-api.appspot.com/random_joke");
-    const data = await res.json();
-    setJoke(data.setup + " - " + data.punchline);
-  };
-
-  const getDog = async () => {
-  const res = await fetch("https://dog.ceo/api/breeds/image/random");
-  const data = await res.json();
-  setDog(data.message);
-};
-
-  useEffect(() => {
-    getCat();
-    getJoke();
-    getDog();
-  }, []);
-
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Frontend Odyssey</h1>
+    <BrowserRouter>
+      <Routes>
 
-      <h2>Pisici</h2>
-      <img src={cat} width="300" />
-      <br />
-      <button onClick={getCat}>Alta pisica</button>
+        <Route path="/" element={<Acasa />} />
 
-      <h2>Glume</h2>
-      <p>{joke}</p>
-      <button onClick={getJoke}>Alta gluma</button>
+        <Route path="/pisici" element={<Pisici />} />
 
-      <h2>Caini</h2>
-      <img src={dog} width="300" />
-      <br />
-      <button onClick={getDog}>Alt caine</button>
-    </div>
+        <Route path="/caini" element={<Caini />} />
+
+        <Route path="/glume" element={<Glume />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
